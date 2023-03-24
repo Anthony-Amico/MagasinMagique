af40b768d6925d0245878da545fea41c77e3f271
Anthony Amico <anthony.amico@le-campus-numerique.fr>
2023-03-24 10:23:45 +0100

    refactoring done

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -9,6 +9,65 @@ class Magasin {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+
+            if (items[i].name.equals("Kryptonite")) {
+                items[i].quality = 80;
+                items[i].sellIn = 9001;
+            }
+            else if (items[i].name.equals("Comté")) {
+                items[i].quality += 1;
+                items[i].sellIn -= 1;
+                if (items[i].sellIn < 0) {
+                    items[i].quality += 1;
+                }
+                if (items[i].quality > 50) {
+                    items[i].quality = 50;
+                }
+            }
+            else if (items[i].name.equals("Pass VIP Concert")) {
+
+                if (items[i].quality > 50) {
+                    items[i].quality = 50;
+                }
+                else if (items[i].sellIn < 0) {
+                    items[i].quality = 0;
+                }
+                else if (items[i].sellIn < 6) {
+                    items[i].quality += 3;
+                }
+                else if (items[i].sellIn < 11) {
+                    items[i].quality += 2;
+                }
+                else {
+                    items[i].quality += 1;
+                    items[i].sellIn -= 1;
+                }
+            }
+            else if (items[i].name.equals("Pouvoirs magiques")) {
+                items[i].quality -= 2;
+                items[i].sellIn -= 1;
+                if (items[i].sellIn < 0) {
+                    items[i].quality -= 2;
+                }
+                if (items[i].quality > 50) {
+                    items[i].quality = 50;
+                }
+            }
+            else {
+                items[i].quality -= 1;
+                items[i].sellIn -= 1;
+                if (items[i].sellIn < 0) {
+                    items[i].quality -= 1;
+                }
+                if (items[i].quality > 50) {
+                    items[i].quality = 50;
+                }
+            }
+        }
+    }
+}
+/*public void updateQuality() {
+        for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Comté")
                     && !items[i].name.equals("Pass VIP Concert")) {
                 if (items[i].quality > 0) {
@@ -58,5 +117,4 @@ class Magasin {
                 }
             }
         }
-    }
-}
+    }*/
--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -6,12 +6,60 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MagasinTest {
 
+    String[] resultExpected = new String[]{
+            "Comté, -1, 17",
+            "Comté, 0, 16",
+            "Comté, -1, 50",
+            "Comté, 0, 50",
+            "Comté, 199, 50",
+            "Comté, 199, 80",
+            "Kryptonite, 10, 50",
+            "Kryptonite, 10, 80",
+            "Kryptonite, 0, 80",
+            "Kryptonite, -1, 80",
+            "Pass VIP Concert, 11, 11",
+            "Pass VIP Concert, 9, 12",
+            "Pass VIP Concert, 4, 13",
+            "Pass VIP Concert, 1, 13",
+            "Pass VIP Concert, 0, 13",
+            "Pass VIP Concert, -1, 0",
+            "mon item, 14, 9",
+            "mon item, 9, 9",
+            "mon item, 4, 9",
+            "mon item, -1, 8"
+    };
+
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] {
+                new Item("Comté", 0, 15),
+                new Item("Comté", 1, 15),
+                new Item("Comté", 0, 50),
+                new Item("Comté", 1, 50),
+                new Item("Comté", 200, 50),
+                new Item("Comté", 200, 80),
+                new Item("Kryptonite", 10, 50),
+                new Item("Kryptonite", 10, 80),
+                new Item("Kryptonite", 0, 80),
+                new Item("Kryptonite", -1, 80),
+                new Item("Pass VIP Concert", 12, 10),
+                new Item("Pass VIP Concert", 10, 10),
+                new Item("Pass VIP Concert", 5, 10),
+                new Item("Pass VIP Concert", 2, 10),
+                new Item("Pass VIP Concert", 1, 10),
+                new Item("Pass VIP Concert", 0, 10),
+                new Item("mon item", 15, 10),
+                new Item("mon item", 10, 10),
+                new Item("mon item", 5, 10),
+                new Item("mon item", 0, 10)
+        };
         Magasin app = new Magasin(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(resultExpected[i] + " <---- resultat attendu");
+            System.out.println(app.items[i]);
+        }
+        assertEquals("Comté", app.items[0].name);
     }
 
 }
